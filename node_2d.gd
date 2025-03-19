b38626bf5e0e2e439c3c6f5ce068bc1487ba00a0
Ricardo-M-Ly <957656884@qq.com>
2025-03-19 17:04:24 +0800

    Add files via upload

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,9 @@
 extends Node2D
 # Called when the node enters the scene tree for the first time.
+var ran1
+var velocity = Vector2i(1,1)
+var rectsx=DisplayServer.screen_get_size().x
+var rectsy=DisplayServer.screen_get_size().y
 func _ready() -> void:
 	get_window().always_on_top=true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,6 +15,20 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move"):
 		var mouse_position =DisplayServer.mouse_get_position()
 		get_window().position=Vector2i(mouse_position.x-50,mouse_position.y-65)
+		print(mouse_position)
 		$AnimatedSprite2D.play("default")
-	else:
-		$AnimatedSprite2D.stop()
+	elif ran1==0:
+		$AnimatedSprite2D.play("stand")
+	elif ran1==1:
+		$AnimatedSprite2D.play("movee")
+		get_window().position += velocity
+		if get_window().position.x > 1910 or get_window().position.x < 0:
+			velocity.x *= -1  # 反转 x 方向速度
+		if get_window().position.y > 1070 or get_window().position.y < 0:
+			velocity.y *= -1  # 反转 y 方向速度
+		print(get_window().position)
+
+
+func _on_timer_timeout() -> void:
+	ran1=randi_range(0,1)
+	print(ran1)
